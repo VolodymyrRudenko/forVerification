ce11e642f927229a12e5e5db10e6c327d6cfafae
da4a <dankossss@gmail.com>
2020-01-15 20:41:35 +0200

    Create the dolphin

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("s§§§§ss.__.ss§§§§§§§§§ss.\n");
+        System.out.print("____________³§§§§§§§§§§§§§§§§§§§§§§s.\n");
+        System.out.print("_____________§§§§§§§§§§§§§§§§§§§§§§§§s\n");
+        System.out.print("_____________§§§§§§§§§§§§§§§§§§O§§§§§s\n");
+        System.out.print("____________s§§§§§§§§§§§§§§§§§§§§§s§§§§§§s\n");
+        System.out.print("___________s§§§§§§§§§§§§§§§§§§³§§§§§³³³³³'\n");
+        System.out.print("__________s§§§§§§§§§§§§§§³³³'\n");
+        System.out.print("__________§§§§§§§§§§§§³³'\n");
+        System.out.print("_________s§§§§§§§§§³³'\n");
+        System.out.print("________s§§§§§§§§³\n");
+        System.out.print("________§§§§§§³'\n");
+        System.out.print("________§§§§§'\n");
+        System.out.print("________§§§§'\n");
+        System.out.print("________§§§'\n");
+        System.out.print("________§§§\n");
+        System.out.print("________§§§§ssss\n");
+        System.out.print("______s§§§§§§§§§§§s,\n");
+        System.out.print("_____s§§§§§§§³³³'\n");
+        System.out.print("____§§§§³³\n");
+    }
+}
